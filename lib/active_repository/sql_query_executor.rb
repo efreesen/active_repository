72e9a3499c9bf2a83cf61d05647de5412e9c451a
Caio Torres <efreesen@gmail.com>
2012-11-05 23:48:57 -0200

    Cleaning code

--- lib/active_repository/sql_query_executor.rb
+++ lib/active_repository/sql_query_executor.rb
@@ -7,53 +7,13 @@ module ActiveHash
         @operator.nil? ? @objects : @objects.send(@operator, execute(klass, @sub_query)).sort_by{ |o| o.id }
       end
 
-      def process_first(klass, query, operator)
-          query = sanitize_query(query)
-
-          array = query.split(" ")
-          sub_query = case operator
-            when "between"
-              array[0..5]
-            when "is"
-              size = array[2].downcase == "not" ? 4 : 3
-              array[0..size]
-            else
-              operator = "==" if operator == "="
-              array[0..3]
-            end
-
-          binding_operator = get_operator(sub_query)
-
-          objects = case operator
-            when "between"
-              klass.all.select{ |o| (o.send(sub_query.first).is_a?(Integer) ? (o.send(sub_query.first) >= sub_query[2].gsub("_", " ").to_i && o.send(sub_query.first) <= sub_query[4].gsub("_", " ").to_i) : (o.send(sub_query.first).to_s) >= sub_query[2].gsub("_", " ") && o.send(sub_query.first) <= sub_query[4].gsub("_", " ")) }
-            when "is"
-              klass.all.select{ |o| sub_query.size == 3 ? o.send(sub_query.first).blank? : !o.send(sub_query.first).blank? }
-            else
-              klass.all.select{ |o| o.send(sub_query.first).nil? ? false : o.send(sub_query.first).to_s.send(operator, sub_query[2].gsub("_", " ")) }
-            end
-
-          sub_query = query.gsub(sub_query.join(" "), "")
-
-          [binding_operator, sub_query, objects]
-      end
-
       def args_to_query(args)
         return args.first if args.size == 1
 
         query = args.first
         param = args.delete(args[1])
 
-        case param.class.name
-        when "String"
-          param = "'#{param}'"
-        when "Date"
-          param = "'#{param.strftime("%Y-%m-%d")}'"
-        when "Time"
-          param = "'#{param.strftime("%Y-%m-%d %H:%M:%S %z")}'"
-        else
-          param = param.to_s
-        end
+        param = convert_param(param)
 
         args[0] = query.sub("?", param)
 
@@ -61,6 +21,44 @@ module ActiveHash
       end
 
       private
+      def process_first(klass, query, operator)
+        @operator = operator == "=" ? "==" : operator
+        @query    = sanitize_query(query)
+        sub_query = divide_query
+
+        binding_operator = get_operator(sub_query)
+
+        objects = execute_sub_query(klass, sub_query)
+
+        sub_query = query.gsub(sub_query.join(" "), "")
+
+        [binding_operator, sub_query, objects]
+      end
+
+      def sanitize_query(query)
+        new_query = query
+        params = query.scan(/([\"'])(.*?)\1/)
+
+        params.each do |quote, param|
+          new_query = new_query.gsub(quote,"").gsub(param, param.gsub(" ", "_"))
+        end
+
+        new_query
+      end
+
+      def divide_query
+        array = @query.split(" ")
+        case @operator
+        when "between"
+          array[0..5]
+        when "is"
+          size = array[2].downcase == "not" ? 4 : 3
+          array[0..size]
+        else
+          array[0..3]
+        end
+      end
+
       def get_operator(attributes)
         operator = attributes.size >= 4 ? attributes.last : nil
 
@@ -74,15 +72,28 @@ module ActiveHash
         end
       end
 
-      def sanitize_query(query)
-        new_query = query
-        params = query.scan(/([\"'])(.*?)\1/)
-
-        params.each do |quote, param|
-          new_query = new_query.gsub(quote,"").gsub(param, param.gsub(" ", "_"))
+      def execute_sub_query(klass, sub_query)
+        case @operator
+        when "between"
+          klass.all.select{ |o| (o.send(sub_query.first).is_a?(Integer) ? (o.send(sub_query.first) >= sub_query[2].gsub("_", " ").to_i && o.send(sub_query.first) <= sub_query[4].gsub("_", " ").to_i) : (o.send(sub_query.first).to_s) >= sub_query[2].gsub("_", " ") && o.send(sub_query.first) <= sub_query[4].gsub("_", " ")) }
+        when "is"
+          klass.all.select{ |o| sub_query.size == 3 ? o.send(sub_query.first).blank? : !o.send(sub_query.first).blank? }
+        else
+          klass.all.select{ |o| o.send(sub_query.first).nil? ? false : o.send(sub_query.first).to_s.send(@operator, sub_query[2].gsub("_", " ")) }
         end
+      end
 
-        new_query
+      def convert_param(param)
+        case param.class.name
+        when "String"
+          param = "'#{param}'"
+        when "Date"
+          param = "'#{param.strftime("%Y-%m-%d")}'"
+        when "Time"
+          param = "'#{param.strftime("%Y-%m-%d %H:%M:%S %z")}'"
+        else
+          param = param.to_s
+        end
       end
     end
   end
