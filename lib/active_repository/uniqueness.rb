193aecabeab69ead78b8efe4c5456dfcd70e38a5
Caio Torres <efreesen@gmail.com>
2014-03-13 11:25:56 -0300

    Fixed uniqueness condition for result_set

--- lib/active_repository/uniqueness.rb
+++ lib/active_repository/uniqueness.rb
@@ -11,7 +11,7 @@ module ActiveModel
       end
 
       def validate_each(record, attribute, value)
-        duplicate = record.get_model_class.where("id != ?", record.id).select do |object|
+        duplicate = record.class.where("id is not ?", (record.id ? record.id : 'null')).all.select do |object|
           object.id != record.id && object.send(attribute) == record.send(attribute)
         end
 
